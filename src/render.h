b2af275acfb6001e77565fee8cab2bafa78d4176
yari :3 <yari_dog@protonmail.com>
2025-10-02 17:03:45 +0100

    i dont know what static does apparently

--- src/render.h
+++ src/render.h
@@ -2,6 +2,6 @@
 #define RENDER_H
 
 #include <stdint.h>
-static void render (uint32_t *buffer, uint32_t size, uint32_t width, uint32_t height);
+void render (uint32_t *buffer, uint32_t size, uint32_t width, uint32_t height);
 
 #endif
