2303659cf684dcd37372cee172dfedd70979144e
yari :3 <yari_dog@protonmail.com>
2025-10-06 14:35:54 +0100

    fixed buffer writing

--- src/render.c
+++ src/render.c
@@ -22,24 +22,21 @@ draw_square (render_context_t *context, uint32_t *surface_buf, uint32_t *input_b
             return;
         }
 
-    uint32_t *current_line = &surface_buf[context->width * y];
-    for (int i = 0; i < height; i++)
-        {
-            memcpy (&current_line[x], &input_buf[i * width], stride);
-            current_line = &current_line[context->width];
-        }
+    surface_buf += context->width * y;
+    for (int i = 0; i < height; ++i)
+        for (int j = 0; j < width; ++j)
+            surface_buf[(i * context->width) + j + x] = input_buf[(i * width) + j];
+
+    // memcpy (&surface_buf[(i * context->width) + x], &input_buf[(i * width)], stride);
 }
 
 void
-draw_checkerboard (render_context_t *context, uint32_t *surface_buffer, uint32_t width, uint32_t height, uint32_t x,
-                   uint32_t y)
+draw_checkerboard (render_context_t *context, uint32_t *surface_buffer, uint32_t bg, uint32_t color, uint32_t width,
+                   uint32_t height, uint32_t x, uint32_t y)
 {
     uint32_t stride    = context->color_depth * width;
     uint32_t *temp_buf = malloc (stride * height);
 
-    // print checkerboard to temp buff
-    uint32_t bg    = 0xFF282828;
-    uint32_t color = 0xFFEBDBB2;
     for (int i = 0; i < height; ++i)
         {
             for (int j = 0; j < width; ++j)
@@ -58,7 +55,7 @@ draw_checkerboard (render_context_t *context, uint32_t *surface_buffer, uint32_t
 
 // TODO: make this work
 void
-draw_color_square (render_context_t *context, uint32_t *surface_buf, uint32_t color, uint32_t width, uint32_t height,
+draw_color_square (render_context_t *context, uint32_t *surface_buf, int32_t color, uint32_t width, uint32_t height,
                    uint32_t x, uint32_t y)
 {
     uint32_t stride    = context->color_depth * width;
@@ -68,24 +65,14 @@ draw_color_square (render_context_t *context, uint32_t *surface_buf, uint32_t co
     draw_square (context, surface_buf, temp_buf, stride, width, height, x, y);
     free (temp_buf);
 }
+
+// TODO: also make this work. i think that im fuckin shit up somewhere
 void
 draw_main_surface (render_context_t *context, uint32_t *surface_buf)
 {
-    memset (surface_buf, 0xFFFFFFFF, context->stride * context->height);
-    // int height    = context->height;
-    // int width     = context->width;
-    // int32_t bg    = 0xFF282828;
-    // int32_t color = 0xFFEBDBB2;
-    // for (int i = 0; i < height; ++i)
-    //     {
-    //         for (int j = 0; j < width; ++j)
-    //             {
-    //                 if ((i + j / 8 * 8) % 16 < 8)
-    //                     surface_buf[i * width + j] = color;
-    //                 // else
-    //                 //     surface_buf[i * width + j] = bg;
-    //             }
-    //     }
+    // set surface to be transparent first.
+    memset (surface_buf, 0x00, context->stride * context->height);
+    draw_color_square (context, surface_buf, 0x1a2b3c4d, context->width, context->height, 0, 0);
 }
 
 void
@@ -95,8 +82,8 @@ render (render_context_t *context, uint32_t *surface_buf)
     draw_main_surface (context, surface_buf);
     draw_color_square (context, surface_buf, 0xFF282828, 80, 80, 0, 0);
     draw_color_square (context, surface_buf, 0xFFEBDBB2, 80, 80, 100, 100);
-    draw_checkerboard (context, surface_buf, 80, 80, 0, 100);
-    draw_checkerboard (context, surface_buf, 80, 80, 100, 0);
+    draw_checkerboard (context, surface_buf, 0xFFEBDBB2, 0xFF282828, 80, 80, 0, 100);
+    draw_checkerboard (context, surface_buf, 0xFF282828, 0xFFEBDBB2, 80, 80, 100, 0);
     INFO ("rendered into buffer ");
 }
 
