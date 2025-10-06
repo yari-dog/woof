10305722f8da741b3104fa15c2cd29d1d5d49fc2
yari :3 <yari_dog@protonmail.com>
2025-10-06 16:26:29 +0100

    forgot to add the config file

---
+++ src/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define COLOR_FORMAT WL_SHM_FORMAT_ARGB8888
+#define COLOR_DEPTH  4
+
+#define WIDTH  800
+#define HEIGHT 400
+#define MARGIN 15
+
+#define INITIAL_COMMAND ":"
+
+#define TITLE ":woof"
+
+#define DOUBLEKEYLIMIT 250 // in ms
+#endif
