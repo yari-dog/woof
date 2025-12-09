9c10c58a542126e3ae168f8634f0a1b88c1240b8
yari :3 <barkbarkbark@yari.dog>
2025-12-09 13:55:01 +0000

    10fps cap

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "util.h"
 #include "woof.h"
 
 int
@@ -7,11 +8,15 @@ main ()
     woof_t *woof = init_woof ();
     woof->start (woof->state);
 
+    int64_t start_time = ms_since_epoch ();
+
     // main loop. entry etc etc etc
     while (!woof->state->close)
-        {
-            woof->main_loop (woof->state);
-        }
+        if (ms_since_epoch () - start_time < 100) // you don't need more than 10fps on a menu
+            {
+                woof->main_loop (woof->state);
+                start_time = ms_since_epoch ();
+            }
 
     // function that handles shutting down gracefully
     destroy_woof (woof);
